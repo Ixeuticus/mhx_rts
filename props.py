ce05e8710a7c24081849aee7ce1f34d9e7fe059f
Thomas Comsol <thomas_larsson_01@hotmail.com>
2022-09-02 04:57:44 +0200

    Introduced limits for float properties.

--- props.py
+++ props.py
@@ -268,14 +268,17 @@ def initMhxProps():
     # Gaze
     bpy.types.Object.MhaGazeFollowsHead = FloatPropOVR(0.0,
         name = "Gaze Follows Head",
+        min = 0, max = 1,
         description = "The gaze bone follows the head bone rotations")
 
     bpy.types.Object.MhaGaze_L = FloatPropOVR(0.0,
         name = "Gaze Left",
+        min = 0, max = 1,
         description = "eye tracking the left gaze bone amount")
 
     bpy.types.Object.MhaGaze_R = FloatPropOVR(0.0,
         name = "Gaze Right",
+        min = 0, max = 1,
         description = "eye tracking the right gaze bone amount")
 
     bpy.types.Object.MhaTongueIk = BoolPropOVR(False,
@@ -285,18 +288,22 @@ def initMhxProps():
     # Hinge
     bpy.types.Object.MhaArmHinge_L = FloatPropOVR(0.0,
         name = "Arm Hinge Left",
+        min = 0, max = 1,
         description = "arm decoupled from the spine rotation")
 
     bpy.types.Object.MhaArmHinge_R = FloatPropOVR(0.0,
         name = "Arm Hinge Right",
+        min = 0, max = 1,
         description = "arm decoupled from the spine rotation")
 
     bpy.types.Object.MhaLegHinge_L = FloatPropOVR(0.0,
         name = "Leg Hinge Left",
+        min = 0, max = 1,
         description = "leg decoupled from the pelvis rotation")
 
     bpy.types.Object.MhaLegHinge_R = FloatPropOVR(0.0,
         name = "Leg Hinge Right",
+        min = 0, max = 1,
         description = "leg decoupled from the pelvis rotation")
 
     # Hands and fingers
@@ -334,53 +341,67 @@ def initMhxProps():
 
     bpy.types.Object.MhaLegIkToAnkle_L = FloatPropOVR(0.0, precision=3,
         name = "Ankle IK Left",
+        min = 0, max = 1,
         description = "Use ankle bone as IK target for left leg")
 
     bpy.types.Object.MhaLegIkToAnkle_R = FloatPropOVR(0.0, precision=3,
         name = "Ankle IK Right",
+        min = 0, max = 1,
         description = "Use ankle bone as IK target for right leg")
 
     bpy.types.Object.MhaArmIk_L = FloatPropOVR(0.0, precision=3,
         name = "Arm IK Left",
+        min = 0, max = 1,
         description = "Left arm IK influence")
 
     bpy.types.Object.MhaArmIk_R = FloatPropOVR(0.0, precision=3,
         name = "Arm IK Right",
+        min = 0, max = 1,
         description = "Right arm IK influence")
 
     bpy.types.Object.MhaLegIk_L = FloatPropOVR(0.0, precision=3,
         name = "Leg IK Left",
+        min = 0, max = 1,
         description = "Left leg IK influence")
 
     bpy.types.Object.MhaLegIk_R = FloatPropOVR(0.0, precision=3,
         name = "Leg IK Right",
+        min = 0, max = 1,
         description = "Right leg IK influence")
 
     # Elbow and Knee parents
     bpy.types.Object.MhaElbowHand_L = FloatPropOVR(0.0,
         name = "Hand>Elbow Left",
+        min = 0, max = 1,
         description = "Parent left elbow pole to hand")
     bpy.types.Object.MhaElbowShoulder_L = FloatPropOVR(0.0,
         name = "Shoulder>Elbow Left",
+        min = 0, max = 1,
         description = "Parent left elbow pole to shoulder")
     bpy.types.Object.MhaElbowHand_R = FloatPropOVR(0.0,
         name = "Hand>Elbow Right",
+        min = 0, max = 1,
         description = "Parent right elbow pole to hand")
     bpy.types.Object.MhaElbowShoulder_R = FloatPropOVR(0.0,
         name = "Shoulder>Elbow Right",
+        min = 0, max = 1,
         description = "Parent right elbow pole to shoulder")
 
     bpy.types.Object.MhaKneeFoot_L = FloatPropOVR(0.0,
         name = "Foot>Knee Left",
+        min = 0, max = 1,
         description = "Parent left knee pole to foot")
     bpy.types.Object.MhaKneeHip_L = FloatPropOVR(0.0,
         name = "Hip>Knee Left",
+        min = 0, max = 1,
         description = "Parent left knee pole to hip")
     bpy.types.Object.MhaKneeFoot_R = FloatPropOVR(0.0,
         name = "Foot>Knee Right",
+        min = 0, max = 1,
         description = "Parent right knee pole to foot")
     bpy.types.Object.MhaKneeHip_R = FloatPropOVR(0.0,
         name = "Hip>Knee Right",
+        min = 0, max = 1,
         description = "Parent right knee pole to hip")
 
     # Elbow and knee parents, changed in edit mode
@@ -413,18 +434,22 @@ def initMhxProps():
     # Stretchiness
     bpy.types.Object.MhaArmStretch_L = FloatPropOVR(0.0,
         name = "Arm Stretch Left",
+        min = 0, max = 1,
         description = "Toggle left arm stretchiness")
 
     bpy.types.Object.MhaLegStretch_L = FloatPropOVR(0.0,
         name = "Leg Stretch Left",
+        min = 0, max = 1,
         description = "Toggle left leg stretchiness")
 
     bpy.types.Object.MhaArmStretch_R = FloatPropOVR(0.0,
         name = "Arm Stretch Right",
+        min = 0, max = 1,
         description = "Toggle right arm stretchiness")
 
     bpy.types.Object.MhaLegStretch_R = FloatPropOVR(0.0,
         name = "Leg Stretch Right",
+        min = 0, max = 1,
         description = "Toggle right leg stretchiness")
 
     bpy.types.Object.MhaToeTarsal_L = BoolProperty(
