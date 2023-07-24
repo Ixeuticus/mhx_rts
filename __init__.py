08a3e1d5bc3e85841e57a238a99268268230a462
Thomas Comsol <thomas_larsson_01@hotmail.com>
2023-07-24 11:03:55 +0200

    Better fix for 1658, using copy transform in pose space for hands and feet.

--- __init__.py
+++ __init__.py
@@ -29,8 +29,8 @@
 bl_info = {
     "name": "MHX Runtime System",
     "author": "Thomas Larsson",
-    "version": (1,7,1),
-    "blender": (3,3,0),
+    "version": (1,7,2),
+    "blender": (3,6,0),
     "location": "UI > MHX",
     "description": "MHX runtime system",
     "warning": "",
